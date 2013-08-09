d9064af9b34876996595b3c140f5fb1fc8d26d2c
David Higginbotham <davehigginbotham@gmail.com>
2013-08-09 08:40:05 -0600

    tests more or less..

--- test/express-rest.js
+++ test/express-rest.js
@@ -13,7 +13,7 @@ app.set('port', 1338);
 app.use(express.methodOverride());
 app.use(express.bodyParser());
 
-var uri = "http://localhost:" + app.get('port');
+var uri = 'http://localhost:' + app.get('port');
 
 var api_path = uri + '/session/v1';
 
@@ -21,14 +21,15 @@ var api_path = uri + '/session/v1';
 var nedbConf = require('../lib').config;
 var nedbMount = require('../lib').mount;
 
-var path = require("path");
+var path = require('path');
 
-var fs = require("fs");
+var fs = require('fs');
 
+var data = require('./data.json');
 var opts = {
-  filePath: path.join(__dirname, "db"),
+  filePath: path.join(__dirname, 'db'),
   maxAge: 1000 * 60 * 60,
-  prefix: "/session"
+  prefix: '/session'
 };
 
 var config = new nedbConf(opts);
@@ -78,7 +79,7 @@ describe('starting restify-nedb test scripts', function () {
 
   it('should add a new document to nedb', function (done) {
 
-    var test = {test: "test posting new object"};
+    var test = {test: 'test posting new object'};
 
     request.post(api_path).send(test).end(function (err, resp) {
       
@@ -93,19 +94,21 @@ describe('starting restify-nedb test scripts', function () {
 
   });
 
-  it('should batch add 100 docs into nedb', function (done) {
+  it('should batch add 250 docs into nedb', function (done) {
 
     var postData = function (index, fn) {
-      request.post(api_path).send({test: "test posting new object"}).end(function (err, resp) {
+      request.post(api_path).send({test: 'test posting new object', _stale: 1000 * 60}).end(function (err, resp) {
         expect(err).to.be(null);
         expect(resp).not.to.be(null);
         expect(resp.statusCode).to.be(200);
-        expect(resp.body.test).to.be("test posting new object");
+        expect(resp.body.test).to.be('test posting new object');
+        expect(resp.body.hasOwnProperty('stale')).not.to.be(false);
+        expect(resp.body.hasOwnProperty('_id')).not.to.be(false);
         fn(null, resp.body);
       });
     };
 
-    async.times(100, function (n, next) {
+    async.times(250, function (n, next) {
       postData(n, function (err, user) {
         next(err,user);
       });
@@ -119,10 +122,11 @@ describe('starting restify-nedb test scripts', function () {
 
   it('should get doc by _id', function (done) {
 
-    request.get(api_path + "/" + id, function(err, resp) {
+    request.get(api_path + '/' + id, function(err, resp) {
 
       expect(err).to.be(null);
       expect(resp.statusCode).to.be(200);
+      expect(resp.body[0]._id).to.be(id);
 
       done();
 
@@ -130,16 +134,126 @@ describe('starting restify-nedb test scripts', function () {
 
   });
 
-  it('should update doc by _id (without append)')
-  it('should update doc by _id (with append)')
-  it('should update doc by _id with custom stale value')
-  it('should delete doc by _id')
-  it('should return 5 docs (with limit)')
-  it('should skip 5 docs (without limit)')
-  it('should return 5 docs skipping first 5 docs (with limit)')
+  it('should update doc by _id (without append)', function (done) {
+
+    request.put(api_path + '/' + id).send({tested: 'updated without append'}).end(function (err, resp) {
+
+      expect(err).to.be(null);
+      expect(resp.statusCode).to.be(200);
+      expect(resp.body._id).to.be(id);
+      expect(resp.body.tested).to.be('updated without append');
+
+      done();
+
+    });
+
+  });
+  
+  it('should update doc by _id (with append)', function (done) {
+
+    request.put(api_path + '/' + id + '?append=true').send({append: 'updated with append'}).end(function (err, resp) {
+
+      expect(err).to.be(null);
+      expect(resp.statusCode).to.be(200);
+      expect(resp.body._id).to.be(id);
+      expect(resp.body.tested).to.be('updated without append');
+      expect(resp.body.append).to.be('updated with append');
+
+      done();
+
+    });
+
+  });
+
+  it('should update doc by _id with custom stale value (with append)', function (done) {
+
+    request.put(api_path + '/' + id + '?append=true').send({_stale: 1000 * 60 * 10}).end(function (err, resp) {
+
+      expect(err).to.be(null);
+      expect(resp.statusCode).to.be(200);
+      expect(resp.body._id).to.be(id);
+      expect(resp.body.tested).to.be('updated without append');
+      expect(resp.body.append).to.be('updated with append');
+
+      done();
+
+    });    
+
+  });
+
+  it('should return 5 docs (with limit)', function (done) {
+
+    request.get(api_path + '?limit=5', function(err, resp) {
+
+      expect(err).to.be(null);
+      expect(resp.statusCode).to.be(200);
+      expect(resp.body.length).to.equal(5);
+      done();
+
+    });    
+
+  });
+
+  it('should skip 5 docs (without limit)', function (done) {
+
+    request.get(api_path + '?skip=5', function(err, resp) {
+
+      expect(err).to.be(null);
+      expect(resp.statusCode).to.be(200);
+      done();
+
+    });    
+
+  });
+
+  it('should return 5 docs skipping first 5 docs (with limit)', function (done) {
+
+    request.get(api_path + '?skip=5&limit=5', function(err, resp) {
+
+      expect(err).to.be(null);
+      expect(resp.statusCode).to.be(200);
+      expect(resp.body.length).to.equal(5);
+      done();
+
+    });    
+
+  });
 
 });
 
-process.on("SIGINT", function () {
+describe('cleanup restify-nedb tests', function () {
+
+  it('should delete doc by _id', function (done) {
+
+    request.del(api_path + '/' + id, function(err, resp) {
+
+      expect(err).to.be(null);
+      expect(resp.statusCode).to.be(200);
+      expect(resp.body.num_deleted).to.be(1);
+      expect(resp.body._id).to.be(id);
+      done();
+
+    });    
+
+  });
+  it('should delete any doc thats stale is later now', function (done) {
+
+
+    config.ds.loadDatabase(function (err) {
+
+      expect(err).to.be(null);
+      config.ds.remove({stale: {$gt: Date.now()}}, {multi: true}, function (err, removed) {
+
+        expect(err).to.be(null);
+        done();
+
+      });
+    });
+
+  });
+
+});
+
+process.on('SIGINT', function () {
   server.exit(0);
 });
